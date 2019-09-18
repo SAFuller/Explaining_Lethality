38c2496265ca3f7e756056ba3ac7ee632f1d507c
SAFuller <46462337+SAFuller@users.noreply.github.com>
2019-09-18 14:31:43 -0500

    Add files via upload

--- Analysis.R
+++ Analysis.R
@@ -1,20 +1,61 @@
-##Analysis
-##Sam Fuller
+##Prelim
 
-#load Packages
+pois<-glm(nonterr_casualties~pch+sq+tch, family = "poisson", data=EDTG)
+summary(pois)
 
-#load data
-data<-read.csv("BAADxGTD.csv")
+nb<-glm.nb(nonterr_casualties~pch+sq+tch, data=EDTG)
+summary(nb)
 
-#prelim
-attacks<-lm(numattack~left+reli+age+sizerec+terrcntrl+leadhierarch, data=data)
-summary(attacks)
 
-kills<-lm(nkill~left+reli+age+sizerec+terrcntrl+leadhierarch, data=data)
-summary(kills)
 
-attacksyear<-lm(numattack~left+reli+year+sizerec+terrcntrl+leadhierarch, data=data)
-summary(attacksyear)
+## Model with controls
+poisful<-glm(nonterr_casualties~pch+tch+sq+terrctrl+lead_hierarch+total_atks+duration+year, family = "poisson", data=EDTG)
+summary(poisful)
+stargazer(poisful, Text=TRUE)
 
-killsyear<-lm(nkill~left+reli+year+sizerec+terrcntrl+leadhierarch, data=data)
-summary(killsyear)
+nbfull<-glm.nb(nonterr_casualties~pch+tch+sq+terrctrl+lead_hierarch+total_atks+duration+year, data=EDTG)
+summary(nbfull)
+
+
+
+
+
+#Model with only age or year
+poisage<-glm(nonterr_casualties~pch+tch+sq+terrctrl+lead_hierarch+total_atks+duration , family = "poisson", data=EDTG)
+summary(poisage)
+
+poisyear<-glm(nonterr_casualties~pch+tch+sq+terrctrl+lead_hierarch+total_atks+year, family = "poisson", data=EDTG)
+summary(poisyear)
+
+
+##Playing with interactions
+
+poisint<-glm(nonterr_casualties~pch+tch+sq+terrctrl+lead_hierarch+total_atks+duration+pch*duration+tch*duration+year, family = "poisson", data=EDTG)
+summary(poisint)
+
+
+
+##Testing goal/hierarchy interactions
+
+goalhier<-glm.nb(nonterr_casualties~policy+policyH+territory+territoryH+squo+squoH+system,  data=EDTG)
+summary(goalhier)
+
+hierCont<-glm.nb(nonterr_casualties~policy+policyH,  data=EDTG)
+summary(hierCont)
+
+stargazer(poisful,nbfull)
+
+
+
+
+##ZINB
+m1 <- zeroinfl(nonterr_casualties ~ pch+tch+lead_hierarch+total_atks 
+               | v2x_freexp_altinf+v2x_frassoc_thick ,
+               data = data2, dist = "negbin", EM = TRUE)
+summary(m1)
+
+m2 <- zeroinfl(nonterr_casualties ~ policy+policyH+territory+territoryH+squo+squoH+system+
+                 lead_hierarch+total_atks 
+               | tpop+cinc+v2x_freexp_altinf+v2x_frassoc_thick ,
+               data = data2 , dist = "negbin", EM = TRUE)
+summary(m2)
--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -1,46 +1,128 @@
-### Data Cleaning script
-### Sam Fuller
+##Sam Fuller
+##EDTG
 
 
-##Load Libraries
+#load packages
+library(readxl)
 library(readr)
-library(countrycode)
-library(magrittr)
+library(data.table)
+library(MASS)
+library(pscl)
+library(zoo)
+library(dplyr)
 library(tidyr)
+library(RcppRoll)
+library(stargazer)
+library(countrycode)
+#load data
+EDTG<- read_excel("EDTG_Data.xls")
 
-##Load BAAD2
-BAAD2_Insurgency_Crime_Dataset <- read_delim("BAAD2 Insurgency Crime Dataset.tab", 
-                                             "\t", escape_double = FALSE, trim_ws = TRUE)
+#Round non-integers
+EDTG$nonterr_casualties<-ceiling(EDTG$nonterr_casualties)
 
-##Load Global Terrorism data base
-gtd<-read_csv("globalterrorismdb.csv", col_types = cols(iyear = col_date(format = "%Y")))
+##Hierarchial Interactions
+EDTG$policyH<-NA
+EDTG$policyH<-(EDTG$pch*EDTG$lead_hierarch)
 
-##Number of attacks/group-year
-gtd1<-aggregate(eventid~gname*iyear, data=gtd, FUN=length)
-names(gtd1)[names(gtd1) == 'eventid'] <- 'numattack'
+EDTG$policy<-NA
+EDTG$policy[EDTG$pch==1 & EDTG$lead_hierarch==0]<-1
+EDTG$policy[EDTG$pch==1 & EDTG$lead_hierarch==1]<-0
+EDTG$policy[EDTG$pch==0]<-0
 
-gtd2<-gtd1 %>%
-  complete(iyear = seq.Date(min(iyear), max(iyear), by="year"),gname)
-gtd2$numattack[is.na(gtd2$numattack)]<-0
+EDTG$territoryH<-NA
+EDTG$territoryH<-(EDTG$tch*EDTG$lead_hierarch)
+
+EDTG$territory<-NA
+EDTG$territory[EDTG$tch==1 & EDTG$lead_hierarch==0]<-1
+EDTG$territory[EDTG$tch==1 & EDTG$lead_hierarch==1]<-0
+EDTG$territory[EDTG$tch==0]<-0
+
+EDTG$squoH<-NA
+EDTG$squoH<-(EDTG$sq*EDTG$lead_hierarch)
+
+EDTG$squo<-NA
+EDTG$squo[EDTG$sq==1 & EDTG$lead_hierarch==0]<-1
+EDTG$squo[EDTG$sq==1 & EDTG$lead_hierarch==1]<-0
+EDTG$squo[EDTG$sq==0]<-0
+
+EDTG$systemH<-NA
+EDTG$systemH<-(EDTG$ercsr*EDTG$lead_hierarch)
+
+EDTG$system<-NA
+EDTG$system[EDTG$ercsr==1 & EDTG$lead_hierarch==0]<-1
+EDTG$system[EDTG$ercsr==1 & EDTG$lead_hierarch==1]<-0
+EDTG$system[EDTG$ercsr==0]<-0
+
+##LOAD COntrols and merge
+
+population <- read_csv("population.csv")
+
+##Reformat World bank data
+rownames(population) <- population$Year
+pop <- setNames(as.data.frame.table(as.matrix(population[-1])),c("cname","year","pop"))
+pop$cname <-rownames(population)
+
+gdp<- read_csv("GDP.csv")
+
+##Reformat world bank data
+rownames(gdp) <- gdp$Year
+GDP <- setNames(as.data.frame.table(as.matrix(gdp[-1])),c("cname","year","gdp"))
+GDP$cname <-rownames(gdp)
+
+##Merge world bank data
+countryf<-merge(pop, GDP, by=c("cname","year"))
+countryf$cc<-countrycode(countryf$cname, "country.name", "cown")
 
 
-#Number of casualties/group-year
-gtd3<-aggregate(nkill~gname*iyear, data=gtd, FUN=sum)
+##Recode west bank and gaza new ccode
+countryf$cc[countryf$cname=="West Bank and Gaza"]<-667
 
-gtd4<-gtd3 %>%
-  complete(iyear = seq.Date(min(iyear), max(iyear), by="year"),gname)
-gtd4$nkill[is.na(gtd4$nkill)]<-0
+##Give serbia a ccode
+countryf$cc[countryf$cname=="Serbia"]<-348
 
-##Merge together attack and killdata
+##Give Puerto Rico a ccode
+countryf$cc[countryf$cname=="Puerto Rico"]<-3
 
-groupyear<-merge(gtd2,gtd4, by=c("gname","iyear"))
+##Fix North Korea
+countryf$cc[countryf$cname=="Korea, Dem. People???s Rep."]<-731
+
+countryf<-na.omit(countryf)
+
+##Load vdem data
+vdem <- read_csv("V-Dem-CY-Core-v9.csv")
+vdem<- subset(vdem, year >1969 & year < 2017)
+vdem<-subset(vdem, select=c("country_name" ,"year" ,"country_text_id",
+                            "v2x_freexp_altinf", "v2xcl_rol", "v2x_frassoc_thick", "v2xeg_eqdr", "v2x_libdem"))
+
+vdem$cc<-countrycode(vdem$country_name, "country.name", "cown")
+vdem$cc[vdem$country_name=="Palestine/West Bank"]<-667
+vdem$cc[vdem$country_name=="Palestine/Gaza"]<-667
+
+##This is to creat a democracy measure for westbank and Gaza together
+vdem3<-subset(vdem, select =-c(country_name, country_text_id))
+vdem2<-aggregate(.~cc+year, data= vdem3, FUN= mean)
+
+##Give serbia a ccode
+vdem$cc[vdem$country_name=="Serbia"]<-348
+
+##Give Puerto Rico a ccode
+vdem$cc[vdem$country_name=="Puerto Rico"]<-3
+
+##Merge data based on primary base
+EDTG$basep<-sapply(strsplit(EDTG$base, ","), '[',1)
+EDTG$cc<-countrycode(EDTG$basep, "country.name", "cown") ###NEED to re-code missing cc
+
+##Create code for Westbank/ Gaza
+EDTG$cc[EDTG$basep=="West Bank/Gaza"]<-667
+##Give serbia a ccode
+EDTG$cc[EDTG$basep=="Serbia"]<-348
+#Give Puerto Rico ccode
+EDTG$cc[EDTG$basep=="Puerto Rico"]<-3
 
 
-##Change year format and merge with BAAD
-groupyear$year<-substr(groupyear$iyear,1,4)%>%as.numeric()
+#Merge Data
+data<-merge(EDTG, vdem, by=c("cc", "year"), all.x = TRUE, all.y=FALSE) 
 
-data<-merge(BAAD2_Insurgency_Crime_Dataset,groupyear, by.x=c("org","year"), by.y=c("gname", "year"))
+data2<-merge(data, countryf, by=c("cc" ,"year"), all.x = TRUE, all.y=FALSE)
 
-##Write Clean data in new .csv
-
-write.csv(data,"BAADxGTD.csv")
+write.csv(data2, "powerorprogress.csv")
